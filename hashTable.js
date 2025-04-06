7a76e4556b89ab3ab1166d390fa3945972aef09d
MahmoudsMohammed <mahmoudsmohammed24@gmail.com>
2025-04-06 06:29:11 +0200

    implement keys method to get all hash table keys

--- hashTable.js
+++ hashTable.js
@@ -27,10 +27,23 @@ class HashTable {
     }
     return undefined;
   }
+
+  keys() {
+    const keys = [];
+    for (let i = 0; i < this.data.length; i++) {
+      // check for bucket is Array and Include items or Not
+      if (Array.isArray(this.data[i]) && this.data[i].length > 0) {
+        const bucketKeys = this.data[i].map((item) => item.key);
+        keys.push(...bucketKeys);
+      }
+    }
+    return keys;
+  }
 }
 
-const myHashTable = new HashTable(2);
+const myHashTable = new HashTable(50);
 myHashTable.set('grapes', 10000);
 myHashTable.set('apples', 9);
 console.log(myHashTable.get('grapes'));
 console.log(myHashTable.get('apples'));
+console.log(myHashTable.keys());
