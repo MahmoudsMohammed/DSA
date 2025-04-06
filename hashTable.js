7e76796a7458d429ff2530781e539ed43302a7a0
MahmoudsMohammed <mahmoudsmohammed24@gmail.com>
2025-04-06 06:44:22 +0200

    solve first recurring character

--- hashTable.js
+++ hashTable.js
@@ -41,9 +41,38 @@ class HashTable {
   }
 }
 
-const myHashTable = new HashTable(50);
-myHashTable.set('grapes', 10000);
-myHashTable.set('apples', 9);
-console.log(myHashTable.get('grapes'));
-console.log(myHashTable.get('apples'));
-console.log(myHashTable.keys());
+// const myHashTable = new HashTable(50);
+// myHashTable.set('grapes', 10000);
+// myHashTable.set('apples', 9);
+// console.log(myHashTable.get('grapes'));
+// console.log(myHashTable.get('apples'));
+// console.log(myHashTable.keys());
+
+//Google Question
+//Given an array = [2,5,1,2,3,5,1,2,4]:
+//It should return 2
+
+//Given an array = [2,1,1,2,3,5,1,2,4]:
+//It should return 1
+
+//Given an array = [2,3,4,5]:
+//It should return undefined
+
+function firstRecurringCharacter(input) {
+  const items = {};
+  if (input.length === 0 || !Array.isArray(input)) {
+    return undefined;
+  } else {
+    for (let i = 0; i < input.length; i++) {
+      if (items[input[i]]) {
+        return input[i];
+      } else {
+        items[input[i]] = 1;
+      }
+    }
+  }
+}
+console.log(firstRecurringCharacter([2, 5, 5, 2, 3, 5, 1, 2, 4]));
+//Bonus... What if we had this:
+// [2,5,5,2,3,5,1,2,4]
+// return 5 because the pairs are before 2,2
