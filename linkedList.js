bf4251f651a2db5af13b670988e83191e8ef8f26
MahmoudsMohammed <mahmoudsmohammed24@gmail.com>
2025-05-03 15:24:54 +0300

    [add] insert functionality to the linked list

--- linkedList.js
+++ linkedList.js
@@ -17,6 +17,31 @@ class LinkedList {
     this.length++;
   }
 
+  insert(index, value) {
+    if (index === 0) {
+      this.prepend(value);
+    } else if (index >= this.length) {
+      this.append(value);
+    } else {
+      const newNode = new Node(value),
+        prev = this.traverseToIndex(index - 1);
+      newNode.next = prev.next;
+      prev.next = newNode;
+      this.length++;
+    }
+  }
+
+  traverseToIndex(index) {
+    let currentNode = this.head,
+      counter = 0;
+    while (counter !== index) {
+      // shift current node to next node & increment counter
+      currentNode = currentNode.next;
+      ++counter;
+    }
+    return currentNode;
+  }
+
   printList() {
     const values = [];
     let currentNode = this.head;
@@ -39,4 +64,6 @@ let myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
+myLinkedList.insert(0, 20);
 console.log(myLinkedList.printList());
+console.log(myLinkedList);
