d43051b990e44ab9a3af872fd9897cc3b94c0ec4
MahmoudsMohammed <mahmoudsmohammed24@gmail.com>
2025-09-22 08:08:03 +0300

    [add] selection & bubble sort algo

--- sorting.js
+++ sorting.js
@@ -1,37 +1,40 @@
 // Bubble Sort
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
-function bubbleSort(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] > arr[j]) {
-        const temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+function bubbleSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
       }
     }
   }
-  return arr;
 }
+// time => O(n^2) => nested loop
+// space => O(1) => const not linked to input increase or decrease
 
-// console.log(bubbleSort(numbers));
+// bubbleSort(numbers);
+// console.log(numbers);
 
 // Selection Sort
-const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
-function selectionSort(arr) {
-  const length = arr.length;
+
+function selectionSort(array) {
+  const length = array.length;
   for (let i = 0; i < length - 1; i++) {
-    let smallestIndex = i;
     for (let j = i + 1; j < length; j++) {
-      if (arr[j] < arr[smallestIndex]) {
-        smallestIndex = j;
+      if (array[j] < array[i]) {
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
       }
     }
-    const temp = arr[i];
-    arr[i] = arr[smallestIndex];
-    arr[smallestIndex] = temp;
   }
-
-  return arr;
+  return array;
 }
+
+// time => O(n^2) => nested loop
+// space => O(1) => const not linked to input increase or decrease 
 
 console.log(selectionSort(numbers));
